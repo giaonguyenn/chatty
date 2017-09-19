17d32cde818f7a48137caa023385f5f0d70c8b4a
Giao Nguyen <giao.nguyenn@yahoo.com>
2017-09-19 15:06:17 -0500

    completed 9/19

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const port = 7000;
+
+const messages = [
+	{
+		messages: "I like front-end."
+	}
+];
+
+const app = express();
+
+app.use(express.static("assets"));
+app.use(bodyParser.json());
+
+app.get("/messages", (req, res, next) => {
+	res.status(200).json({messages: messages});
+});
+
+app.post("/messages", (req, res, next) => {
+	messages.push({message: req.body.message, time: new Date()});
+	res.status(200).json({messages: messages});
+});
+
+app.listen(port, () => {console.log("blah")});
